d40affc2148a10ba9490c400b078ee76194cb0b1
Ihor-91 <ihorstarafun@gmail.com>
2022-07-27 13:37:14 +0300

    added header and slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,16 @@
+import $ from "jquery";
+
+import "slick-carousel";
+
 import "../scss/main.scss";
 import "../index.html";
+
+//slider
+$(".slider__block").slick({
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  dots: true,
+  speed: 2000,
+  zIndex: 0,
+});
